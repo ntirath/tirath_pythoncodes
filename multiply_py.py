a7d2bea929faf186dd56c40908a84f3090f3d573
ntirath <ntirath@gmail.com>
2022-07-11 18:30:24 +0530

    Update multiply_py.py

--- multiply_py.py
+++ multiply_py.py
@@ -1,5 +1,6 @@
 #Program for Multiplication
 a = int(input("Input A = "))
 b = int(input("Input B = "))
-c = a * b
-print("Addition of a & b = ", c)
+c = float(input("Input C = "))
+d = a * b * c
+print("Addition of a & b = ", d)
