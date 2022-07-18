6a259700a2f5d5c4834d3eef6b60d2183d74fec3
ntirath <ntirath@gmail.com>
2022-07-18 18:07:36 +0530

    Update prg_180722.py

--- prg_180722.py
+++ prg_180722.py
@@ -22,11 +22,36 @@ print("Resultant string3 = ", string3)
 list1 = ['tirath','prasad',34,100.345]
 print("First element of list1:", list1[0])
 print("Third element of list1:", list1[3])
+
+#Multidimensional list
 list2 = ['nagvekar',list1,'Rohit']
 print("List 2 = ", list2)
 print("Third component of List1 embedded in List2 :", list2[2][3]) # wrong answer
 print("Third component of List1 embedded in List2 :", list2[1][3]) # correct answer
+
+list3 = [0,0,0]
+list3[0] = "Tirath"
+list3[1] = "Prasad"
+list3[2] = "Nagvekar"
+
+print("List3 = ", list3)
+
+#Tuple
 Tuple1 = (1,2,3)
 Tuple2 = (0.1,0.2,0.3)
 Tuple3 = Tuple1 + Tuple2
 print("Tuple 1 = ", Tuple1)
+print("resultant tuple is concetenated tuple:", Tuple3)
+Tuple4 = (Tuple1, Tuple2)
+print("resultant tuple is the nested tuple:", Tuple4)
+print("3rd element of tuple2 in typle4 :", Tuple4[1][2])
+Tuple5 = Tuple3 + Tuple4
+print("Resultant Tuple5:", Tuple5)
+
+tuple6 =(0,0,0)
+tuple6[0] = "Tirath"
+tuple6[1] = "Prasad"
+tuple6[2] = "Nagvekar"
+
+print("tuple6 = ", tuple6)
+
