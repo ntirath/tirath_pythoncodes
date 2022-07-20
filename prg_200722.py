b9deaab941daef6679de3e3da13b70208602b591
ntirath <ntirath@gmail.com>
2022-07-20 17:26:52 +0530

    Create prg_200722.py

---
+++ prg_200722.py
@@ -0,0 +1,14 @@
+# calculate a6
+a = int(input("Enter value of a = "))
+b = int(input("Enter value of b = "))
+
+c1 = a**2
+c2 = b**2
+c3 = 2*a*b
+
+d1 = (a+b)**2
+print("The answer for (a+b)**2", d1)
+d2 = (a**2) + (2*a*b) + (b**2)
+print("The answer for (a**2) + (2*a*b) + (b**c)", d2)
+d3 = c1 + c3 + c2
+print("The answer for a^2 + b^2 + 2ab using separately calculated values", d3)
