3434c62e1e8ec3046d2f9a8bb50546c2d3e899fb
ntirath <ntirath@gmail.com>
2022-08-01 19:27:40 +0530

    class_program

--- prg_010822.py
+++ prg_010822.py
@@ -26,7 +26,17 @@
 # for i in range(len(list)):
 #     print(list[i], end = " ")
 
-for var in "Python world":
-    if var == ' ':
-        continue
-    print(var, end = " ")
+# for var in "Python world":
+#     if var == ' ':
+#         continue
+#     print(var, end = " ")
+
+l1 = ['eat','sleep','repeat']
+s1 = 'python'
+obj1 = enumerate(l1)
+obj2 = enumerate(s1)
+
+print("Return type :", type(obj1))
+print(list(enumerate(l1)))
+
+print(list(enumerate(s1,2)))
