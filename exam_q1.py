0226b2efa09e8ec415cd0c33b2bdbeb5a4389180
ntirath <ntirath@gmail.com>
2022-08-15 19:01:59 +0530

    Update exam_q1.py

--- exam_q1.py
+++ exam_q1.py
@@ -19,3 +19,25 @@ for i in range(m):
 print("The value of the number", n)
 print("The value of the power", p)
 print("Power of the list = ", res)
+
+#Q1 : Power of the list using recursion
+
+def pow_num(n,p):
+    if p == 0:
+        return 1
+    else:
+        return (n*(pow_num(n,p-1)))
+
+#n = int(input("Enter the value of the number , n = "))
+#p = int(input("Enter the value of the power , p = "))   
+n = [0, 1, 2, 4, 10, 23, 30, 43, 52, 67, 78, 100]
+res =[]
+p = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+m = len(n)
+for i in range(m):
+    result= pow_num(n[i],p[i])
+    res.append(result)
+#result = pow_num(n,p)
+print("The value of the number", n)
+print("The value of the power", p)
+print("Power of the list = ", res)
