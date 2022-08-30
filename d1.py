8ca68d3b8180722f1fba2f30e08dd29f520d129e
ntirath <ntirath@gmail.com>
2022-08-30 18:40:37 +0530

    30th aug

---
+++ d1.py
@@ -0,0 +1,8 @@
+list1 = [ 1, 2, 4, 3]
+list2 = [ 1, 2, 5, 8]
+list3 = [ 1, 2, 5, 8, 10]
+list4 = [ 1, 2, 4, 3]
+  
+# Comparing lists 
+print("Comparison of list2 with list1 : ")
+print(cmp(list2, list1))
--- s1.py
+++ s1.py
@@ -2,8 +2,8 @@
 m = int(input("Enter the value of which fibonnaci series to be generated, a = "))
 n =[]
 
-def lambda_z():
-    return lambda f,g : f + g
+#def lambda_z():
+#    return lambda f,g : f + g
 
 for i in range(0,m):
     if (i == 0):
@@ -15,8 +15,8 @@ for i in range(0,m):
     elif (i >= 2):
         f = n[i-2]
         g = n[i-1]
-        c = lambda_z()
-        res=c(f,g)
+        #c = lambda_z()
+        res= lambda f,g : f + g
         n.append(res)
 print(n)
 
